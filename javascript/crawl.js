bf2e84b4d11cff0f92d068e8bdfff44a2478ee01
Tapomay <tapomay.dey@gmail.com>
2016-01-26 17:29:27 -0800

    Formatting

--- javascript/crawl.js
+++ javascript/crawl.js
@@ -1,6 +1,13 @@
 //Crawl PDF filenames from an html page.
 //Run following code on dev console of browser
-var pdflinks =[]; Array.prototype.map. call(document.querySelectorAll("a[href$=\".pdf\"]"), function(e, i){if((pdflinks||[]).indexOf(e.href)==-1){ pdflinks.push( e.href);} }); console.log(pdflinks.join(" "));
+
+var pdflinks = [];
+Array.prototype.map.call(document.querySelectorAll("a[href$=\".pdf\"]"), function(e, i) {
+	if ((pdflinks || []).indexOf(e.href) == -1) {
+		pdflinks.push(e.href);
+	}
+});
+console.log(pdflinks.join(" "));
 
 //http://superuser.com/questions/260087/download-all-pdf-links-in-a-web-page
 
